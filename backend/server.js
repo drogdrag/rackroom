3061312c88cb1de10f4e478f113be0f771710968
Phuripat22 <fps.10620382@gmail.com>
2026-09-17 10:57:38 +0700

    delete const PORT = process.env.PORT || 3000;

--- backend/server.js
+++ backend/server.js
@@ -272,6 +272,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Backend running at http://localhost:${PORT}`);
 });
-
-// ให้ใช้ Port ที่ Render กำหนดมาให้ แต่ถ้าไม่มีให้ใช้ 3000
-const PORT = process.env.PORT || 3000;
